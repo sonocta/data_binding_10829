43b293a8a1935babe0f948ebeed5da6f5db7f2dd
sonocta <octakainakaimu23@gmail.com>
2022-09-13 10:59:30 +0700

    Initial commit

---
+++ app/src/main/java/com/octa/data_binding_10829/FakeRepository.kt
@@ -0,0 +1,29 @@
+package com.octa.data_binding_10829
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import kotlin.random.Random
+
+object FakeRepository {
+    private val fruitNames: List<String> = listOf(
+        "Apel", "mangga", "Pisang", "Anggur", "Stroberi",
+        "Jambu", "Raspberi", "Bambu", "Kiwi", "Pir"
+    )
+
+    private val _currentRandomFruitName = MutableLiveData<String>()
+    val currentRandomFruitName: LiveData<String>
+        get() = _currentRandomFruitName
+
+    init {
+        _currentRandomFruitName.value = fruitNames.first()
+    }
+
+    fun getRandomfruitName(): String {
+        val random = Random.nextInt(fruitNames.size)
+        return fruitNames[random]
+    }
+
+    fun changeCurrentRandomFruitName() {
+        _currentRandomFruitName.value = getRandomfruitName()
+    }
+}
